e6bcc9af47259c7761b24bc40fa898378de63386
Juan <juanpabloagudeloperez@pascualbravo.edu.co>
2022-03-25 20:18:10 -0500

    uploaded

--- js/app.js
+++ js/app.js
@@ -1,7 +1,21 @@
+
+/*JavaScript*/
+
+
 const ham = document.querySelector('.hamburger');
 const enlaces = document.querySelector('.links');
 const bar = document.querySelectorAll('.hamburger span')
 ham.addEventListener('click', ()=>{
     enlaces.classList.toggle('active');
     bar.forEach(child =>{child.classList.toggle('animate')})
+});
+
+
+/*JQuery*/
+
+$(function () {
+    $('.title').addClass('LtR');
+    $('#main-nav img').addClass('LtR');
+    $('.links').addClass('RtL');
+    $('.hamburger').addClass('RtL');
 });
