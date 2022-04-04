fd5ed3989b1dfb65eac478ef1ccd55e38540c1e4
Shibui Dev <73392970+ShibuiDev@users.noreply.github.com>
2022-04-04 12:28:19 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 
 /*JavaScript*/
 
-
 const ham = document.querySelector('.hamburger');
 const enlaces = document.querySelector('.links');
 const bar = document.querySelectorAll('.hamburger span')
+const body = document.querySelector('body');
 ham.addEventListener('click', ()=>{
     enlaces.classList.toggle('active');
-    bar.forEach(child =>{child.classList.toggle('animate')})
+    bar.forEach(child =>{child.classList.toggle('animate')});
+    body.classList.toggle('no-overflow')
 });
-
 
 /*JQuery*/
 
